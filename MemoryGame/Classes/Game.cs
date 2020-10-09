ed110c23a5f43a5313bada01f4e65e5a547c97cc
Psychopath24 <m.hooijberg24@gmail.com>
2020-10-09 19:37:16 +0100

    Added:   - Documentation to functions   - Card class, to save data of specific cards.   - CardCollection to the game class to save a card field.
    Changed:
      - Enhanced multiple functions to look cleaner and take up less space.

--- MemoryGame/Classes/Game.cs
+++ MemoryGame/Classes/Game.cs
@@ -14,13 +14,24 @@ namespace MemoryGame.Classes
         public TimeSpan PlayerTime1 { get; set; }
         public TimeSpan PlayerTime2 { get; set; }
 
+        public Card[] CardCollection { get; set; }
+
         public Game.PlayerTurn Turn { get; set; }
 
         public enum PlayerTurn { Player1, Player2 }
-
+        
         public Game(GameConfig config)
         {
             Config = config;
         }
+    }
+
+    public class Card
+    {
+        public bool IsTurned { get; set; }
+        public Uri FrontImageURI { get; set; }
+        public Uri BackImageURI { get; set; }
+        public int Row { get; set; }
+        public int Column { get; set; }
     }
 }
--- MemoryGame/UserControls/UserControl_GameField.xaml.cs
+++ MemoryGame/UserControls/UserControl_GameField.xaml.cs
@@ -28,10 +28,12 @@ namespace MemoryGame.UserControls
         private DispatcherTimer timer;
 
         public TimeSpan time;
+
         public UserControl_GameField(Game game)
         {
-            _game = game;
             InitializeComponent();
+            _game = game;
+
             time = new TimeSpan(0, 0, 0);
             timer = new DispatcherTimer();
             timer.Interval = new TimeSpan(0, 0, 0, 0, 1000); 
@@ -41,51 +43,11 @@ namespace MemoryGame.UserControls
             //TODO: per player timer.s
         }
 
-        private void UserControl_Loaded(object sender, RoutedEventArgs e)
-        {
-            var window = Window.GetWindow(this);
-            window.KeyDown += KeyPressHandler;
-
-            SetupPlayfield(_game.Config.FieldHeight, _game.Config.FieldWidth);
-            SetNames(new string[] { _game.Config.PlayerName1, _game.Config.PlayerName2});
-        }
-
-        private void SetNames(string[] names)
-        {
-            lbl_player1Name.Content = names[0];
-            lbl_player2Name.Content = names[1];
-        }
-
-        private void SetupPlayfield(int height, int width)
-        {
-            for (int i = 0; i < height; i++)
-            {
-                grd_cardGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(grd_cardGrid.Height / height) });
-            }
-
-            for (int i = 0; i < width; i++)
-            {
-                grd_cardGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(grd_cardGrid.Width / width)});
-            }
-
-            grd_cardGrid.ShowGridLines = true;
-        }
-
-
-        // Controls what code 
-        private void KeyPressHandler(object sender, KeyEventArgs e)
-        {
-            switch(e.Key)
-            {
-                case Key.Escape:
-                    TogglePauseMenu();
-                    break;
-
-                case Key.W:
-                    KeyPressW();
-                    break;
-            }
-        }
+        #region UserControl Functions
+        /// <summary>
+        /// Toggles pause menu visibility and timer.
+        /// By Mark Hooijberg
+        /// </summary>
         private void TogglePauseMenu()
         {
             if (grd_pauseMenu.Visibility == Visibility.Hidden)
@@ -100,46 +62,82 @@ namespace MemoryGame.UserControls
                 // Resume timer etc.
                 timer.Start();
                 grd_pauseMenu.Visibility = Visibility.Hidden;
-
             }
         }
-        private void KeyPressW()
-        {
-            _game.PlayerScore1 = Convert.ToInt32(lbl_player1Score.Content.ToString().Split(' ')[1]);
-            _game.PlayerScore2 = Convert.ToInt32(lbl_player2Score.Content.ToString().Split(' ')[1]);
-            Content = new UserControl_EndScreen(_game);
 
+        /// <summary>
+        /// Assigns names to the player name labels.
+        /// By Mark Hooijberg
+        /// </summary>
+        /// <param name="names">Sequence of names to be set.</param>
+        private void SetNames(string[] names)
+        {
+            lbl_player1Name.Content = names[0];
+            lbl_player2Name.Content = names[1];
         }
 
-        #region PauseMenuButtonEvents
-        // Knoppen om terug naar het hoofdmenu te gaan.
-        private void Btn_Quit_Click(object sender, RoutedEventArgs e)
+        /// <summary>
+        /// Create a grid for the cards to be loaded in with a given size.
+        /// By Mark Hooijberg.
+        /// </summary>
+        /// <param name="rows">Number of rows</param>
+        /// <param name="columns">Number of columns</param>
+        private void SetupPlayfield(int rows, int columns)
         {
-            Content = new UserControl_MainMenu();
+            for (int i = 0; i < rows; i++)
+                grd_cardGrid.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(grd_cardGrid.Height / rows) });
+
+            for (int i = 0; i < columns; i++)
+                grd_cardGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(grd_cardGrid.Width / columns)});
         }
-        // Hieronder geschreven door Jur Stedehouder
+        #endregion
+
+        #region Event Handlers
+        /// <summary>
+        /// Configures the PlayingField.
+        /// By: Mark Hooijberg.
+        /// </summary>
+        private void UserControl_Loaded(object sender, RoutedEventArgs e)
+        {
+            var window = Window.GetWindow(this);
+            window.KeyDown += KeyPressHandler;
+
+            SetupPlayfield(_game.Config.FieldHeight, _game.Config.FieldWidth);
+            SetNames(new string[] { _game.Config.PlayerName1, _game.Config.PlayerName2});
+        }
+
+        /// <summary>
+        /// Fired when clicked on Continue in pause menu. Closes the pause menu.
+        /// By: Jur Stedehouder
+        /// Enhanced by: Mark Hooijberg
+        /// </summary>
+        private void Btn_Continue_Click(object sender, RoutedEventArgs e) => TogglePauseMenu();
+
+        /// <summary>
+        /// Return to main menu.
+        /// Written and enhanced by: Mark Hooijberg
+        /// Implemented by: Jur Stedehouder
+        /// </summary>
+        private void Btn_Quit_Click(object sender, RoutedEventArgs e) => Content = new UserControl_MainMenu();
+
+        /// <summary>
+        /// Save the state of the game and return to main menu.
+        /// By: Jur Stedehouder
+        /// </summary>
         private void Btn_Save_Click(object sender, RoutedEventArgs e)
         {
             // TO DO: Put code to save game here.
             Content = new UserControl_MainMenu();
         }
 
-        private void Btn_Continue_Click(object sender, RoutedEventArgs e) => TogglePauseMenu();
-        #endregion
-
-        //Code hieronder geschreven door Jur Stedehouder gecontroleerd door Mark Hooijberg
-        
-        private void dtClockTime_Tick(object sender, EventArgs e)
-        {
-            time += new TimeSpan(0, 0, 1);
-            klok.Content = time.Duration().ToString(@"mm\:ss");
-        }
-
+        /// <summary>
+        /// Dynamically changes the size of the card grid.
+        /// By: Mark Hooijberg
+        /// </summary>
         private void UserControl_SizeChanged(object sender, SizeChangedEventArgs e)
         {
             grd_cardGrid.Height = e.NewSize.Height - 150;
             grd_cardGrid.Width = grd_cardGrid.Height;
-            grd_cardGrid.Margin = new Thickness(0, 75, 0, 0);
 
             foreach (RowDefinition rowDefinition in grd_cardGrid.RowDefinitions)
                 rowDefinition.Height = new GridLength(grd_cardGrid.Height / grd_cardGrid.RowDefinitions.Count());
@@ -147,5 +145,48 @@ namespace MemoryGame.UserControls
             foreach (ColumnDefinition columnDefinition in grd_cardGrid.ColumnDefinitions)
                 columnDefinition.Width = new GridLength(grd_cardGrid.Width / grd_cardGrid.ColumnDefinitions.Count());
         }
+
+        /// <summary>
+        /// Updates adds a second to the time and time lable.
+        /// Originally witten by: Jur Stedehouder
+        /// Enhanced by: Mark Hooijberg
+        /// </summary>
+        private void dtClockTime_Tick(object sender, EventArgs e)
+        {
+            time += new TimeSpan(0, 0, 1);
+            klok.Content = time.Duration().ToString(@"mm\:ss");
+        }
+
+        /// <summary>
+        /// Executes different functions based on the key which is pressed
+        /// By: Mark Hooijberg
+        /// /// </summary>
+        /// <param name="e">Arguments collected by the event.</param>
+        private void KeyPressHandler(object sender, KeyEventArgs e)
+        {
+            switch(e.Key)
+            {
+                case Key.Escape:
+                    TogglePauseMenu();
+                    break;
+
+                case Key.W:
+                    KeyPressW();
+                    break;
+            }
+        }
+
+        /// <summary>
+        /// Save score in the game object and overwrites the content with a new End Screen object.
+        /// This is to simulate a game ending.
+        /// By: Mark Hooijberg
+        /// </summary>
+        private void KeyPressW()
+        {
+            _game.PlayerScore1 = Convert.ToInt32(lbl_player1Score.Content.ToString().Split(' ')[1]);
+            _game.PlayerScore2 = Convert.ToInt32(lbl_player2Score.Content.ToString().Split(' ')[1]);
+            Content = new UserControl_EndScreen(_game);
+        }
+        #endregion
     }
 }
